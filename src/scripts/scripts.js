46ae84d65954773bc9b820b44b426c9adf662806
Izael silva <izaell.oficial@gmail.com>
2023-12-08 16:47:37 -0300

    Update scripts.js

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,13 +1,33 @@
+const html = document.documentElement
+const cache = localStorage.getItem('mode')
+
+if (cache === 'light') {
+  html.classList.add(cache)
+}
+
+function setMode(mode) {
+  localStorage.setItem('mode', mode);
+}
+
+function resetMode() {
+  localStorage.removeItem('mode')
+}
+
+function getMode() {
+  return localStorage.getItem('mode')
+}
+
 function toggleMode() {
-  const html = document.documentElement
   html.classList.toggle('light')
 
   const imgProfile = document.querySelector('#profile img')
 
   if (html.classList.contains('light')) {
+    setMode('light')
     imgProfile.setAttribute('src', 'https://github.com/ias4g.png')
     imgProfile.setAttribute('alt', 'Imagem de Izael com terno preto, camisa preta, cabelo preto, pele parda e barba curta com o fundo branco.')
   } else {
+    setMode(null)
     imgProfile.setAttribute('src', 'https://www.github.com/ias4g.png')
     imgProfile.setAttribute('alt', 'Imagem de Izael com terno preto, camisa preta, cabelo preto, pele parda e barba curta com o fundo branco.')
   }
