8b620be1f2db0779bbd1c3891d16237c64dcdfb6
Izael silva <izaell.oficial@gmail.com>
2023-12-07 21:08:35 -0300

    Update scripts.js

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -2,13 +2,13 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle('light')
 
-  const img = document.querySelector('#profile img')
+  const imgProfile = document.querySelector('#profile img')
 
   if (html.classList.contains('light')) {
-    img.setAttribute('src', './src/assets/avatar.png')
-    img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo')
+    imgProfile.setAttribute('src', 'https://github.com/ias4g.png')
+    imgProfile.setAttribute('alt', 'Imagem de Izael com terno preto, camisa preta, cabelo preto, pele parda e barba curta com o fundo branco.')
   } else {
-    img.setAttribute('src', 'https://www.github.com/ias4g.png')
-    img.setAttribute('alt', 'Imagem de Izael com terno preto, camisa preta, cabelo preto, pele parda e barba curta com o fundo branco.')
+    imgProfile.setAttribute('src', 'https://www.github.com/ias4g.png')
+    imgProfile.setAttribute('alt', 'Imagem de Izael com terno preto, camisa preta, cabelo preto, pele parda e barba curta com o fundo branco.')
   }
 }
